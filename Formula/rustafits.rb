5f037a6884c54ede0c44eaf9354e56e04af36348
eg013ra1n <vilen.sharifov@gmail.com>
2026-02-26 21:58:39 +0200

    Update rustafits to 0.4.3 with correct sha256

--- Formula/rustafits.rb
+++ Formula/rustafits.rb
@@ -2,7 +2,7 @@ class Rustafits < Formula
   desc "High-performance FITS/XISF to JPEG converter with auto-stretch"
   homepage "https://github.com/eg013ra1n/rustafits"
   url "https://github.com/eg013ra1n/rustafits/archive/v0.4.3.tar.gz"
-  sha256 "CHECKSUM_HERE"
+  sha256 "b2a11fc9ccbec004171d816baa801142180ce8afae5f6eb665d4c86059a0e7de"
   license "Apache-2.0"
 
   depends_on "rust" => :build
